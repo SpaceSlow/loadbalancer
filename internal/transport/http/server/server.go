d0c96eb1d6ee61dde7f8071e8a58f2dad2f96bd7
Igor Ivanov <igor.ivanov.it@mail.ru>
2025-05-10 22:29:01 +0700

    feat(ratelimiter): add http server with clients api endpoints

---
+++ internal/transport/http/server/server.go
@@ -0,0 +1,50 @@
+package server
+
+import (
+	"context"
+	"fmt"
+	"log/slog"
+	"net/http"
+
+	"github.com/SpaceSlow/loadbalancer/config"
+	"github.com/SpaceSlow/loadbalancer/internal/transport/http/balancer"
+	"github.com/SpaceSlow/loadbalancer/internal/transport/http/clients"
+	"github.com/SpaceSlow/loadbalancer/internal/transport/http/ratelimiter"
+)
+
+type Server struct {
+	server         *http.Server
+	clientHandlers clients.ClientHandlers
+}
+
+func NewServer(ctx context.Context, cfg *config.Config, clientService clients.ClientService) (*Server, error) {
+	const basePathLayout = "/api/v1%s"
+
+	mux := http.NewServeMux()
+
+	clientHandlers := clients.NewClientHandlers(clientService)
+
+	mux.Handle(fmt.Sprintf(basePathLayout, "/clients/"), clientHandlers.Clients())
+	mux.Handle(fmt.Sprintf(basePathLayout, "/clients/{id}/"), clientHandlers.ClientByID())
+
+	b, err := balancer.NewBalancer(ctx, &cfg.Balancer)
+	if err != nil {
+		slog.Error("Create balancer error occurred", slog.String("error", err.Error()))
+		return nil, err
+	}
+	mux.Handle("/", b.Handler())
+
+	limiter := ratelimiter.NewRateLimiter(ctx, &cfg.RateLimiter)
+
+	slog.Info("Starting balancer", slog.Int("port", cfg.Balancer.Port))
+	server := &http.Server{
+		Addr:    fmt.Sprintf(":%d", cfg.Balancer.Port),
+		Handler: limiter.Middleware(mux),
+	}
+
+	return &Server{server: server}, nil
+}
+
+func (s *Server) Start() error {
+	return s.server.ListenAndServe()
+}
