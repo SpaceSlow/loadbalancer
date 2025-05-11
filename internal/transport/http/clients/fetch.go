3c13ecdd39ed8217f9cb987f83c879c9df73ba21
Igor Ivanov <igor.ivanov.it@mail.ru>
2025-05-11 16:07:11 +0700

    feat(clients): implement fetch client handler

--- internal/transport/http/clients/fetch.go
+++ internal/transport/http/clients/fetch.go
@@ -1,7 +1,34 @@
 package clients
 
-import "net/http"
+import (
+	"errors"
+	"log/slog"
+	"net/http"
+
+	"github.com/SpaceSlow/loadbalancer/internal/domain/clients"
+	"github.com/SpaceSlow/loadbalancer/internal/transport/http/dto"
+	"github.com/SpaceSlow/loadbalancer/pkg/httpjson"
+)
 
 func (h *ClientHandlers) FetchClient(w http.ResponseWriter, r *http.Request, clientID string) {
-	// TODO implement
+	client, err := h.service.Fetch(r.Context(), clientID)
+	if errors.Is(err, clients.ErrClientNotExists) {
+		dto.WriteErrorResponse(w, http.StatusNotFound, "User with this username not exists")
+		return
+	} else if err != nil {
+		slog.Error(
+			"Fetch client error",
+			slog.String("error", err.Error()),
+			slog.String("client_id", clientID),
+		)
+		httpjson.WriteJSON(w, http.StatusInternalServerError, nil)
+		return
+	}
+
+	response := dto.Client{
+		ClientID: client.ID,
+		Capacity: client.Capacity,
+		RPS:      client.RPS,
+	}
+	httpjson.WriteJSON(w, http.StatusOK, response)
 }
