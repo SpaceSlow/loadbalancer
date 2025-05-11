cfd3db4387f69d541d4d79261be3aabb28be2ecd
Igor Ivanov <igor.ivanov.it@mail.ru>
2025-05-11 16:03:32 +0700

    feat(clients): implement delete client handler

--- internal/domain/clients/errors.go
+++ internal/domain/clients/errors.go
@@ -5,3 +5,4 @@ import "errors"
 var ErrCapacityValidation = errors.New("validation error: capacity must be >= 1")
 var ErrRPSValidation = errors.New("validation error: RPS must be > 0")
 var ErrClientExists = errors.New("client already exists")
+var ErrClientNotExists = errors.New("client not exists")
--- internal/transport/http/clients/delete.go
+++ internal/transport/http/clients/delete.go
@@ -1,7 +1,29 @@
 package clients
 
-import "net/http"
+import (
+	"errors"
+	"log/slog"
+	"net/http"
+
+	"github.com/SpaceSlow/loadbalancer/internal/domain/clients"
+	"github.com/SpaceSlow/loadbalancer/internal/transport/http/dto"
+	"github.com/SpaceSlow/loadbalancer/pkg/httpjson"
+)
 
 func (h *ClientHandlers) DeleteClient(w http.ResponseWriter, r *http.Request, clientID string) {
-	// TODO implement
+	err := h.service.Delete(r.Context(), clientID)
+	if errors.Is(err, clients.ErrClientNotExists) {
+		dto.WriteErrorResponse(w, http.StatusConflict, "User with this username not exists")
+		return
+	} else if err != nil {
+		slog.Error(
+			"Delete client error",
+			slog.String("error", err.Error()),
+			slog.String("client_id", clientID),
+		)
+		httpjson.WriteJSON(w, http.StatusInternalServerError, nil)
+		return
+	}
+
+	httpjson.WriteJSON(w, http.StatusNoContent, nil)
 }
